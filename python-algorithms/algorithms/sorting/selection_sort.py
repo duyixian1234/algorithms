18380521379fbd171c1b11f831c719e3cdfdd060
duyixian <duyixian1234@qq.com>
2020-11-22 20:14:00 +0800

    fix selection sort

--- python-algorithms/algorithms/sorting/selection_sort.py
+++ python-algorithms/algorithms/sorting/selection_sort.py
@@ -6,7 +6,7 @@ DataT = TypeVar("DataT")
 def selection_sort(arr: List[DataT]):
     length = len(arr)
     for i in range(length):
-        smallest = arr[i]
+        smallest = i
         for j in range(i + 1, length):
             if arr[smallest] > arr[j]:
                 smallest = j
